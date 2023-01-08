23000ce5da63677d3239113de36785cab7fe8632
idrizp <idrizpelaj@hotmail.com>
2023-01-08 14:57:00 +0100

    Added README

---
+++ README.md
@@ -0,0 +1,20 @@
+# Homefinder
+
+My first attempt at trying to use ChatGPT as an intelligent search api and failing miserably.
+Onto next week's project though, gonna take the lessons learned from this(namely, never to use NextJS's beta again) and see what happens.
+
+Watch this space ;)!
+
+# Where host? Where can I check it out?
+
+Unfortunately, you can't. The ChatGPT api I'm using requires a headful browser. I do not have the bank to get a server with a GUI.
+
+# Mistakes made along the way:
+
+- Assuming OpenAPI had a public chatgpt api and not having to use hacks.. wrong.
+- Assuming NextJS beta's /app/ folder would be stable.. again, wrong.
+
+# Lessons learned:
+
+- If I want the experience of NextJS beta, i'll just use sveltekit instead
+- If I want to use ChatGPT.. I'm gonna have to finagle a lot with the existing api wrappers and make my own(..watch this space?)
--- homefinder-processor/index.js
+++ homefinder-processor/index.js
@@ -24,38 +24,56 @@ app.get("/:query", async (req, res) => {
   let { query } = req.params;
   query = decodeURIComponent(query);
   query = query.slice(0, 180) + ".";
-  const response = await chatgpt.sendMessage(
-    `Extract one keyword related to house size; one showing the number of rooms as an integer; one for the precise location; one for the price as an integer; one for the currency of the price as its currency code, all separated by a semicolon from the following message: ${query}`
-  );
-  console.log(response);
-  // get the text response
-  const text = response.response;
-  const result = text?.split(";");
-  if (result.length < 6 || result.length > 6) {
+  let sent = false;
+  let timeout = false;
+  setTimeout(() => {
+    if (!sent) {
+      res.status(500).json({ error: "Timed out." });
+      timeout = true;
+    }
+  }, 10_000);
+  try {
+    const response = await chatgpt.sendMessage(
+      `Extract one keyword related to house size; one showing the number of rooms as an integer; one for the precise location; one for the price as an integer; one for the currency of the price as its currency code, all separated by a semicolon from the following message: ${query}`
+    );
+    if (timeout) {
+      return;
+    }
+    console.log(response);
+    // get the text response
+    const text = response.response;
+    const result = text?.split(";");
+    if (result.length < 6 || result.length > 6) {
+      res.status(200).json({
+        houseSize: "N/A",
+        location: "N/A",
+        price: -1,
+        priceCurrency: "USD",
+      });
+      return;
+    }
+
+    sent = true;
+    const outputLines = result?.map((line) =>
+      line.split(":")[1].replace(" ", "")
+    );
+    const price = parseInt(outputLines?.[3] || "0");
+    const priceCurrency = outputLines?.[4];
+
+    console.log(outputLines);
+
+    await chatgpt.refreshSession();
+
     res.status(200).json({
-      houseSize: "N/A",
-      roomNumber: -1,
-      location: "N/A",
-      price: -1,
-      priceCurrency: "USD",
+      houseSize: outputLines?.[0] || "N/A",
+      roomNumber: parseInt(outputLines?.[1] || "0"),
+      location: outputLines?.[2] || "N/A",
+      price: price,
+      priceCurrency: priceCurrency || "USD",
     });
-    return;
+  } catch (e) {
+    res.status(500).json({
+      error: "The server failed to process your request.",
+    });
   }
-
-  const outputLines = result?.map((line) =>
-    line.split(":")[1].replace(" ", "")
-  );
-  const price = parseInt(outputLines?.[3] || "0");
-  const priceCurrency = outputLines?.[4];
-
-  console.log(outputLines);
-
-  await chatgpt.refreshSession();
-  res.status(200).json({
-    houseSize: outputLines?.[0] || "N/A",
-    roomNumber: parseInt(outputLines?.[1] || "0"),
-    location: outputLines?.[2] || "N/A",
-    price: price,
-    priceCurrency: priceCurrency || "USD",
-  });
 });
